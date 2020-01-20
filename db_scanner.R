6076f5f81a3a0bf8dbbdee471be61e1481556018
raivolalala <30519997+raivolalala@users.noreply.github.com>
2020-01-20 23:07:13 +0200

    Added MySQL database scanner written in R

---
+++ db_scanner.R
@@ -0,0 +1,34 @@
+# contact me at lallo.raivo@gmail.com!
+library(RMySQL)
+library(tictoc)
+
+# Enter your own user, password, database and host address!
+connection = dbConnect(
+  MySQL(),
+  user = '',
+  password = '',
+  dbname = '',
+  host = '')
+
+tables.list <- dbListTables(connection)
+for (i in tables.list) {
+  sink("your_text_file.txt", split = TRUE, append = TRUE)
+  cat('\n############################################# NEW SCAN, TABLE:', i)
+  my.table = dbSendQuery(
+    connection, paste0("select * from ", i)
+  )
+  my.table <- fetch(my.table, n = -1)
+  
+  cat("\n Your table colnames:\n")
+  print(names(my.table[,order(colnames(my.table))]))
+  toc(log = TRUE, quiet = FALSE)
+  tic(msg = 'Time elapsed for retrieving blanks: ')
+  missing.from.my.table <- sapply(my.table, function(x) sum(is.na(x) | x==""))/nrow(my.table)
+  cat("\n Columns with blanks more than 80%:\n")
+  print(names(missing.from.my.table[missing.from.my.table > 0.8]))
+  toc(log = TRUE, quiet = FALSE)
+  tic(msg = 'Time elapsed for getting colnames: ')
+  sink()
+}
+
+lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
