180d08e126767ee72e92a3803d539ad19a628750
raivolalala <lallo.raivo@gmail.com>
2020-10-17 11:11:44 +0300

    some stuff again

--- convert_sql_excel.R
+++ convert_sql_excel.R
@@ -1,12 +1,12 @@
 library(RODBC)
 
-sqlServer <- "raivoclout.database.windows.net"  #Enter Azure SQL Server
-sqlDatabase <- "fromPS"                #Enter Database Name
-sqlUser <- "admraivo"             #Enter the SQL User ID
-sqlPassword <- "$dmR4!v0"        #Enter the User Password
-sqlDriver <- "SQL Server"        #Leave this Drive Entry
+sqlServer <- c("raivoclout.database.windows.net")	#Enter Azure SQL Server
+sqlDatabase <- c("fromPS") 	               		#Enter Database Name
+sqlUser <- c("admraivo")             			#Enter the SQL User ID
+sqlPassword <- c("$dmR4!v0")        			#Enter the User Password
+sqlDriver <- c("SQL Server")        			#Leave this Drive Entry
 
-connectionStringSQL <- paste0(
+conn_string <- paste0(
   "Driver=", sqlDriver,
   ";Server=", sqlServer,
   ";Database=", sqlDatabase,
@@ -15,8 +15,9 @@ connectionStringSQL <- paste0(
   ";Encrypt=yes",
   ";Port=1433")
 
-sql_query <-  c('SELECT * FROM dbo."f_iss_coordinates";')
+sql_query <- c('SELECT * FROM dbo."f_iss_coordinates";')
 
-conn <- odbcConnect(connectionStringSQL)
+conn <- odbcConnect(conn_string)
 sqlDataFrame <- sqlQuery(conn, sqlQuery)
 close(conn)
+csv.write(sqlDataFrame,  'iss_coordinates.csv')
