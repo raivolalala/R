1be4e333db3968e47d5ff260cdec8e17419e5aac
raivolalala <lallo.raivo@gmail.com>
2020-10-24 23:35:19 +0300

    still learning

--- machine_learning.R
+++ machine_learning.R
@@ -8,11 +8,25 @@ validation.index <- createDataPartition(my.data$Species,
 validation <- my.data[-validation.index,]
 dataset <- my.data[validation.index,]
 
-# split input and output
+# Define input and output
 x <- dataset[,1:4]
 y <- dataset[,5]
+################################################ Visuals start
 
-par(mfrow=c(1,4))
-for(i in 1:4) {
-	boxplot(x[,i], main = names(iris)[i])
-}
+# Univariate plots
+# Boxplot for each attribute on one image
+#par(mfrow=c(1,4))
+#for(i in 1:4) {
+#	boxplot(x[,i], main = names(iris)[i])
+#}
+
+# Barplot for class breakdown
+#plot(y)
+
+# Multivariate plots. TODO: THE BELOW DOESN'T FUCKING WORK!! NOTHING!!!
+# Scatterplot matrix
+#featurePlot(x=x, y=y, plot="ellipse")
+
+# Box and whisker plots for each attribute
+featurePlot(x=x, y=y, plot="box")
+################################################ Visuals end
