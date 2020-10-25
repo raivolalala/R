6bbf297bfed62e49f2ac66031a3eb3e778d52a73
raivolalala <lallo.raivo@gmail.com>
2020-10-25 08:30:57 +0200

    blablablabla

--- machine_learning.R
+++ machine_learning.R
@@ -13,7 +13,7 @@ x <- dataset[,1:4]
 y <- dataset[,5]
 ################################################ Visuals start
 
-# Univariate plots
+##################### Univariate plots
 # Boxplot for each attribute on one image
 #par(mfrow=c(1,4))
 #for(i in 1:4) {
@@ -23,11 +23,15 @@ y <- dataset[,5]
 # Barplot for class breakdown
 #plot(y)
 
-# Multivariate plots. TODO: THE BELOW DOESN'T FUCKING WORK!! NOTHING!!!
+##################### Multivariate plots
+# TODO: ok, featurePlot works when wrapped with print(), dunno why.
+# In RStudio it doesn't need print().
+
 # Scatterplot matrix
-#featurePlot(x=x, y=y, plot="ellipse")
+# TODO: below func breaks.
+#print(featurePlot(x=x, y=y, plot="ellipse"))
 
 # Box and whisker plots for each attribute
 print(featurePlot(x=x, y=y, plot="box"))
-plot(dataset)
+
 ################################################ Visuals end
