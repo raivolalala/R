8021c24b80ac0f9a7fe1ada9b4d3a5aab2c34e14
raivolalala <lallo.raivo@gmail.com>
2020-10-25 21:32:43 +0200

    nothing of an interest here...

--- machine_learning.R
+++ machine_learning.R
@@ -1,14 +1,13 @@
 library(caret)
 data(iris)
 
-my.data <- iris
-validation.index <- createDataPartition(my.data$Species,
+dataset <- iris
+validation.index <- createDataPartition(dataset$Species,
 					p = 0.8,
 					list = FALSE)
-validation <- my.data[-validation.index,]
-dataset <- my.data[validation.index,]
+validation <- dataset[-validation.index,]
+dataset <- dataset[validation.index,]
 
-# Define input and output
 x <- dataset[,1:4]
 y <- dataset[,5]
 ################################################ Visuals start
@@ -24,16 +23,14 @@ y <- dataset[,5]
 #plot(y)
 
 ##################### Multivariate plots
-# TODO: ok, featurePlot for "box" works when wrapped with print(), dunno why.
-
 # Scatterplot matrix
-# Func below breaks...
+# TODO: Func below breaks...
 #print(featurePlot(x=x, y=y, plot="ellipse", auto.key=list(columns=3)))
 
-# Box and whisker plots for each attribute
+# TODO: works only when put in print atrocity...
 print(featurePlot(x=x, y=y, plot="box"))
 
-# Below func returns NULL...
+# TODO: Below func returns NULL...
 #print(featurePlot(x=x, y=y, plot="scatter"))
 
 ################################################ Visuals end
